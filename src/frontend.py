eddb8bd2e377bb82d95f349b28a0345e7bc73dc5
Luciano Filho <lvgalvaofilho@gmail.com>
2024-01-27 10:42:59 -0300

    fix: melhoria do validor de schema

--- src/frontend.py
+++ src/frontend.py
@@ -10,7 +10,7 @@ class ExcelValidadorUI:
         )
         
     def display_header(self):
-        st.title("Nao gosto do meu cliente")
+        st.title("Validador de schema excel")
 
     def upload_file(self):
         return st.file_uploader("Carregue seu arquivo Excel aqui", type=["xlsx"])
