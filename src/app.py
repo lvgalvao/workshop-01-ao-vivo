5131b3618c885822fe2c6f828a96d3db5c14344f
Luciano Filho <lvgalvaofilho@gmail.com>
2024-01-29 15:39:35 -0300

    update

--- src/app.py
+++ src/app.py
@@ -1,5 +1,18 @@
 from frontend import ExcelValidadorUI
 from backend import process_excel, excel_to_sql
+import logging
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://6cee69d21063f4615b44dcf164ce9527@o4505699197452288.ingest.sentry.io/4506644154417152",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 def main():
     ui = ExcelValidadorUI()
@@ -13,9 +26,13 @@ def main():
 
         if error:
             ui.display_wrong_message()
+            logging.error("Planilha apresentava erro de schema")
+            sentry_sdk.capture_message("A planilha Excel estava errada")
         elif ui.display_save_button():
             excel_to_sql(df)
             ui.display_success_message()
+            logging.info(" Foi enviado com sucesso o banco SQL")
+            sentry_sdk.capture_message("O banco SQL foi atualizado")
 
 if __name__ == "__main__":
     main()
